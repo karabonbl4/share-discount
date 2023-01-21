06b9c8c9d5de1c0d85ea408a59604cdcdcac0992
karabonbl4 <karabonbl4@gmail.com>
2023-01-21 16:04:36 +0300

    homework-3-ioc prefinal_else_little_bit

--- di/src/main/java/Main.java
+++ di/src/main/java/Main.java
@@ -1,22 +1,7 @@
-import com.senla_ioc.context.ApplicationContext;
-import com.senla_ioc.context.ObjectFactory;
-import com.senla_ioc.context.PropertyScanner;
-import com.senla_ioc.context.Scanner;
-import com.senla_ioc.context.impl.AnnotationApplicationContext;
-import com.senla_ioc.test.TestInterface;
-import com.senla_ioc.test.TestValueAnnotation;
-import org.reflections.Reflections;
-
-import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationApplicationContext();
-        applicationContext.buildContext("com.senla_ioc.test");
-
-        System.out.println(applicationContext.getBean(TestValueAnnotation.class).toString());
-        System.out.println(applicationContext.getBeans().toString());
 
     }
 }
