f364e6cbe206edb38cde00f81764d0bfd99cb311
karabonbl4 <karabonbl4@gmail.com>
2023-02-01 11:33:06 +0300

    homework-4-spring-ioc fvth upd

---
+++ src/main/source/sql/08.create-coupon-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS public.coupon
+(
+    id 					BIGSERIAL,
+    name 				VARCHAR,
+    start_date 			DATE NOT NULL,
+    end_date 			DATE NOT NULL,
+    discount 			DECIMAL(3, 2) DEFAULT 0,
+    used 				BOOLEAN DEFAULT FALSE,
+    trademark_id 		BIGINT NOT NULL,
+    CONSTRAINT coupon_id_pk PRIMARY KEY (id),
+    CONSTRAINT coupon_trademark_fk FOREIGN KEY (trademark_id)
+    	REFERENCES public.trademark (id) MATCH SIMPLE,
+    CONSTRAINT coupon_discount_check CHECK (discount >=0 and discount <= 1)
+);
